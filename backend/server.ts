dbd7c8d6a87b298b75a91d271b11c1bd056a6ef4
kagerer19 <kagerer19@gmail.com>
2024-07-13 10:11:38 +0200

    Creat SignOn and SignUp features

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,6 @@ import { Application } from "express";
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-
 export const app: Application = express();
 
 // Import routes
@@ -11,17 +10,14 @@ const createGameRoute = require('./routes/gameRoutes');
 const deleteGameRoute = require('./routes/gameRoutes');
 const updateGameRoute = require('./routes/gameRoutes');
 const showGameRoute = require('./routes/gameRoutes');
-
-// Import locationRouts
-const createLocationRoute = require('./routes/locationRoutes');
-const showLocationsRoute = require('./routes/locationRoutes');
-const showLocationRoute = require('./routes/locationRoutes');
-const deleteLocationRoute = require('./routes/locationRoutes');
-const updateLocationRoute = require('./routes/locationRoutes');
 const showSignUpRoute = require('./routes/authRoutes');
 const showSignInRoute = require('./routes/authRoutes');
 const showSignOutRoute = require('./routes/authRoutes');
+
+// Import locationRouts
 const locationRoutes = require('./routes/locationRoutes');
+
+//!TODO Add cors
 
 // Middleware
 app.use(bodyParser.json({limit: "15mb"}));
@@ -30,25 +26,18 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// Add morgan for logs
+// add morgan for logs
 app.use(morgan('combined'));
 app.use('/api', showGamesRoute)
 app.use('/api', createGameRoute)
 app.use('/api', deleteGameRoute)
 app.use('/api', updateGameRoute)
 app.use('/api', showGameRoute)
-
-app.use('/api', createLocationRoute)
-app.use('/api', showLocationsRoute)
-app.use('/api', showLocationRoute)
-app.use('/api', deleteLocationRoute)
-app.use('/api', updateLocationRoute)
-//signOn
 app.use('/api', showSignUpRoute)
 app.use('/api', showSignInRoute)
 app.use('/api', showSignOutRoute)
-//locations
-app.use('/api',locationRoutes)
+app.use('/api', locationRoutes);
+
 
  const port = 8000 || 9000
  app.listen(port, () => {
