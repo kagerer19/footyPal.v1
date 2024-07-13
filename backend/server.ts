645d39f397624fd52ba1fd4243a9c1e71cfd92a7
kagerer19 <kagerer19@gmail.com>
2024-07-13 10:11:38 +0200

    Creat SignOn and SignUp features

--- backend/server.ts
+++ backend/server.ts
@@ -2,36 +2,48 @@ import { Application } from "express";
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-
 export const app: Application = express();
 
 // Import routes
-const gameRoutes = require('./routes/gameRoutes');
-const authRoutes = require('./routes/authRoutes');
+const showGamesRoute = require('./routes/gameRoutes');
+const createGameRoute = require('./routes/gameRoutes');
+const deleteGameRoute = require('./routes/gameRoutes');
+const updateGameRoute = require('./routes/gameRoutes');
+const showGameRoute = require('./routes/gameRoutes');
+const showSignUpRoute = require('./routes/authRoutes');
+const showSignInRoute = require('./routes/authRoutes');
+const showSignOutRoute = require('./routes/authRoutes');
+
+// Import locationRouts
 const locationRoutes = require('./routes/locationRoutes');
 
+//!TODO Add cors
+
 // Middleware
-app.use(bodyParser.json({ limit: "15mb" }));
+app.use(bodyParser.json({limit: "15mb"}));
 app.use(bodyParser.urlencoded({
     limit: "15mb",
     extended: true
 }));
 
-// Add morgan for logs
+// add morgan for logs
 app.use(morgan('combined'));
-
-// Game routes
-app.use('/api', gameRoutes);
-
-// Auth routes
-app.use('/api', authRoutes);
-
-// Location routes
+app.use('/api', showGamesRoute)
+app.use('/api', createGameRoute)
+app.use('/api', deleteGameRoute)
+app.use('/api', updateGameRoute)
+app.use('/api', showGameRoute)
+app.use('/api', showSignUpRoute)
+app.use('/api', showSignInRoute)
+app.use('/api', showSignOutRoute)
 app.use('/api', locationRoutes);
 
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
+
+ const port = 8000 || 9000
+ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
-module.exports = app;
+ module.exports = app
+
+
