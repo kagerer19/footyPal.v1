1f8187959cb35eb472c9758eee97e7a08c7fd299
Marcel Roblek <revc0@Marcels-MacBook-Pro.local>
2024-07-13 08:15:47 -0700

    Resolved Merge Conflict on Rebase

--- backend/server.ts
+++ backend/server.ts
@@ -1,52 +1,37 @@
-import {Application} from "express";
-
+import { Application } from "express";
 const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require("morgan")
-export const app:Application = express();
+const morgan = require("morgan");
+
+export const app: Application = express();
 
 // Import routes
-const showGamesRoute = require('./routes/gameRoutes');
-const createGameRoute = require('./routes/gameRoutes');
-const deleteGameRoute = require('./routes/gameRoutes');
-const updateGameRoute = require('./routes/gameRoutes');
-const showGameRoute = require('./routes/gameRoutes');
-
-// Import locationRouts
-const createLocationRoute = require('./routes/locationRoutes');
-const showLocationsRoute = require('./routes/locationRoutes');
-const showLocationRoute = require('./routes/locationRoutes');
-const deleteLocationRoute = require('./routes/locationRoutes');
-const updateLocationRoute = require('./routes/locationRoutes');
-
-//!TODO Add cors
+const gameRoutes = require('./routes/gameRoutes');
+const authRoutes = require('./routes/authRoutes');
+const locationRoutes = require('./routes/locationRoutes');
 
 // Middleware
-app.use(bodyParser.json({limit: "15mb"}));
+app.use(bodyParser.json({ limit: "15mb" }));
 app.use(bodyParser.urlencoded({
     limit: "15mb",
     extended: true
 }));
 
-// add morgan for logs
+// Add morgan for logs
 app.use(morgan('combined'));
-app.use('/api', showGamesRoute)
-app.use('/api', createGameRoute)
-app.use('/api', deleteGameRoute)
-app.use('/api', updateGameRoute)
-app.use('/api', showGameRoute)
 
-app.use('/api', createLocationRoute)
-app.use('/api', showLocationsRoute)
-app.use('/api', showLocationRoute)
-app.use('/api', deleteLocationRoute)
-app.use('/api', updateLocationRoute)
+// Game routes
+app.use('/api', gameRoutes);
 
- const port = 8000 || 9000
- app.listen(port, () => {
+// Auth routes
+app.use('/api', authRoutes);
+
+// Location routes
+app.use('/api', locationRoutes);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
- module.exports = app
-
-
+module.exports = app;
